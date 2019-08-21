5139976d4184f151eeceaaf3533d1503d6a70656
Nirothipan <nirothipan@gmail.com>
2019-08-21 15:05:58 +0530

    Update osgi version for kernel 4.5

--- modules/runtime-introspection/setup.sh
+++ modules/runtime-introspection/setup.sh
@@ -12,11 +12,11 @@ echo Modifying the OSGI environment...
 
 
 PLUGIN_DIR=$1/repository/components/plugins
-unzip -qo $PLUGIN_DIR/org.eclipse.osgi_3.9.1.v20130814-1242.jar -d $PLUGIN_DIR/org.eclipse.osgi_3.9.1.v20130814-1242
-cp $PLUGIN_DIR/org.eclipse.osgi_3.9.1.v20130814-1242.jar $PLUGIN_DIR/..
-rm $PLUGIN_DIR/org.eclipse.osgi_3.9.1.v20130814-1242.jar
+unzip -qo $PLUGIN_DIR/org.eclipse.osgi_3.14.0.v20190517-1309.jar -d $PLUGIN_DIR/org.eclipse.osgi_3.14.0.v20190517-1309
+cp $PLUGIN_DIR/org.eclipse.osgi_3.14.0.v20190517-1309.jar $PLUGIN_DIR/..
+rm $PLUGIN_DIR/org.eclipse.osgi_3.14.0.v20190517-1309.jar
 cd ./osgi-classes/target/classes
-find . -type f -name "*.class" | cpio -pdv $PLUGIN_DIR/org.eclipse.osgi_3.9.1.v20130814-1242 \;
+find . -type f -name "*.class" | cpio -pdv $PLUGIN_DIR/org.eclipse.osgi_3.14.0.v20190517-1309 \;
 echo Modifying the OSGI environment Done
 
 echo You can run the normal wso2server.sh with -Dwso2.osgi.class.loader.intro.name=xx option now.
